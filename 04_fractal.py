b2e3c30020742c4e45f42f2a6cb6d1366f98cd62
Krul <krul1wtd@yandex.ru>
2022-09-22 19:26:57 +0700

    fractal

--- 04_fractal.py
+++ 04_fractal.py
@@ -2,6 +2,27 @@
 
 import simple_draw as sd
 
+root = sd.get_point(300, 0)
+
+
+def draw_branches(point, angle, length):
+    if length < 10:
+        return
+    root_vector_1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
+    root_vector_1.draw()
+    next_vector_1 = root_vector_1.end_point
+    next_length_1 = length * (sd.random_number(75, 85) / 100)
+    next_angle_1 = angle - sd.random_number(10, 30)
+    next_angle_1_1 = angle + sd.random_number(15, 45)
+    draw_branches(point=next_vector_1, angle=next_angle_1, length=next_length_1)
+    draw_branches(point=next_vector_1, angle=next_angle_1_1, length=next_length_1)
+    # root_vector_2 = sd.get_vector(start_point=root, angle=angle, length=length, width=3)
+    # root_vector_2.draw()
+
+
+draw_branches(root, 90, 100)
+
+
 # 1) Написать функцию draw_branches, которая должна рисовать две ветви дерева из начальной точки
 # Функция должна принимать параметры:
 # - точка начала рисования,
