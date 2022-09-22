f3d4ecd94c5ccd824cc7dae6408f28c46bcfa5d6
Krul <krul1wtd@yandex.ru>
2022-09-22 16:30:21 +0700

    shapes

--- 01_shapes.py
+++ 01_shapes.py
@@ -4,15 +4,72 @@ import simple_draw as sd
 
 # Часть 1.
 # Написать функции рисования равносторонних геометрических фигур:
-# - треугольника
-# - квадрата
-# - пятиугольника
-# - шестиугольника
-# Все функции должны принимать 3 параметра:
-# - точка начала рисования
-# - угол наклона
-# - длина стороны
+
+point_0 = sd.get_point(300, 300)
+point_1 = sd.get_point(150, 300)
+point_2 = sd.get_point(150, 150)
+point_3 = sd.get_point(300, 150)
+
+
+# def triangle(point, angle, length):
+#     v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
+#     v1.draw(color=sd.COLOR_RED)
+#     v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 120, length=length, width=3)
+#     v2.draw(color=sd.COLOR_RED)
+#     v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 240, length=length, width=3)
+#     v3.draw(color=sd.COLOR_RED)
 #
+#
+# triangle(point=point_0, angle=0, length=100)
+
+
+def triangle_func(point, angle, length):
+    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
+    v1.draw(color=sd.COLOR_RED)
+    next_angle = angle + 120
+    if next_angle > 240:
+        return
+    triangle_func(point=v1.end_point, angle=next_angle, length=length)
+
+
+triangle_func(point=point_0, angle=0, length=100)
+
+
+def square(point, angle, length):
+    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
+    v1.draw(color=sd.COLOR_RED)
+    next_angle = angle + 90
+    if next_angle > 270:
+        return
+    square(point=v1.end_point, angle=next_angle, length=length)
+
+
+square(point=point_1, angle=0, length=100)
+
+
+def five_angles(point, angle, length):
+    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
+    v1.draw(color=sd.COLOR_RED)
+    next_angle = angle + 72
+    if next_angle > 288:
+        return
+    five_angles(point=v1.end_point, angle=next_angle, length=length)
+
+
+five_angles(point=point_2, angle=0, length=75)
+
+
+def six_angles(point, angle, length):
+    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
+    v1.draw(color=sd.COLOR_RED)
+    next_angle = angle + 60
+    if next_angle > 320:
+        return
+    six_angles(point=v1.end_point, angle=next_angle, length=length)
+
+
+six_angles(point=point_3, angle=0, length=75)
+
 # Использование копи-пасты - обязательно! Даже тем кто уже знает про её пагубность. Для тренировки.
 # Как работает копипаста:
 #   - одну функцию написали,
@@ -27,7 +84,6 @@ import simple_draw as sd
 # sd.line()
 # Результат решения см lesson_004/results/exercise_01_shapes.jpg
 
-# TODO здесь ваш код
 
 # Часть 1-бис.
 # Попробуйте прикинуть обьем работы, если нужно будет внести изменения в этот код.
